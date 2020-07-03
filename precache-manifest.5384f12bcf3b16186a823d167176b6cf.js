dcca684a67bfdee6bc210f7072c54b96048b2b28
Adeyeye Olushola <adeyeyeolushola977@gmail.com>
2020-07-03 04:58:02 +0100

    Updates

---
+++ precache-manifest.5384f12bcf3b16186a823d167176b6cf.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d797fb3f217db5d23a8dd3c8b49ecbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d27be99e087c82c4f9f3",
+    "url": "/static/css/main.9aa2bb01.chunk.css"
+  },
+  {
+    "revision": "3dbae2e843a38e9dbb50",
+    "url": "/static/js/2.4da4c765.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4da4c765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d27be99e087c82c4f9f3",
+    "url": "/static/js/main.5fd45944.chunk.js"
+  },
+  {
+    "revision": "26aed8ffd8e7c0363a89",
+    "url": "/static/js/runtime-main.f56b52a4.js"
+  },
+  {
+    "revision": "9f72c13bc10a76e4bcb8f7ab62582b9f",
+    "url": "/static/media/account.9f72c13b.svg"
+  },
+  {
+    "revision": "6e2f7196c0644447b6887a48a265bdb1",
+    "url": "/static/media/bg-header-desktop.6e2f7196.svg"
+  },
+  {
+    "revision": "39eaebdf1b82df9648392a9fb0915e9c",
+    "url": "/static/media/eyecam-co.39eaebdf.svg"
+  },
+  {
+    "revision": "76faa79fba7225014706c0aac0b46472",
+    "url": "/static/media/faceit.76faa79f.svg"
+  },
+  {
+    "revision": "52b85a5018745489a50626498bc347ad",
+    "url": "/static/media/insure.52b85a50.svg"
+  },
+  {
+    "revision": "6c50fd745d8d5c5e1dd47fa8c9029a6b",
+    "url": "/static/media/loop-studios.6c50fd74.svg"
+  },
+  {
+    "revision": "fbe431a9e5d624447d698fee099ca843",
+    "url": "/static/media/manage.fbe431a9.svg"
+  },
+  {
+    "revision": "e0ad9358d008658c14d4350d48627923",
+    "url": "/static/media/myhome.e0ad9358.svg"
+  },
+  {
+    "revision": "3f34b49c96dcbb44450b43184cd6cd78",
+    "url": "/static/media/photosnap.3f34b49c.svg"
+  },
+  {
+    "revision": "45f63772b29b7ccba3bc0c0c6e1d505d",
+    "url": "/static/media/shortly.45f63772.svg"
+  },
+  {
+    "revision": "23b515f95978bcb24d704b1abecc8196",
+    "url": "/static/media/the-air-filter-company.23b515f9.svg"
+  }
+]);
